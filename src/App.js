bdec24854b8fb1373af57555d85de5ebdabeac6e
R, Chaitra <chaitra.r@legatohealth.com>
2020-05-25 04:47:44 -0400

    added next file

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
       );
     return (
       <div className='App'>
+      <h1> Monster RoloDex</h1>
       <SearchBox 
          placeholder='search monsters' 
          handleChange= { e => this.setState ({searchFields : e.target.value})}/>
